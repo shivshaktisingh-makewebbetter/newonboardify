a91c975cd0ded390013500d696b597c331394678
shivshaktisingh-cedcommerce <shivshaktisingh@cedcommerce.com>
2024-07-23 18:00:18 +0530

    fix

--- src/user/TrackDetailsPage.jsx
+++ src/user/TrackDetailsPage.jsx
@@ -115,9 +115,12 @@ export const TrackDetails = () => {
       const tempData = JSON.parse(subItem.data);
 
       if (item.id === tempData.column_id) {
+       if(!tempData.column_type === 'long_text'){
         if (tempData.hasOwnProperty("value") && tempData.value !== null) {
           tempInitialAction = tempData.value.label.text;
         }
+       }
+        
         break;
       }
     }
@@ -134,9 +137,11 @@ export const TrackDetails = () => {
       const tempData = JSON.parse(subItem.data);
 
       if (item.id === tempData.column_id) {
+        if(!tempData.column_type === 'long_text'){
         if (tempData.hasOwnProperty("value") && tempData.value !== null) {
           tempColor = tempData.value.label.style.color;
         }
+      }
         break;
       }
     }
