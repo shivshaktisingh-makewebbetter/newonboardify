5d278bab3a852d916f49d7572fa07d520bc7a01c
shivshaktisingh-cedcommerce <shivshaktisingh@cedcommerce.com>
2024-07-24 15:36:26 +0530

    fixes

--- src/routeWrapper/ProtectedRoute.js
+++ src/routeWrapper/ProtectedRoute.js
@@ -1,24 +1,28 @@
 // ProtectedRoute.js
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { getRole } from '../utils/helper';
 
 export const ProtectedRoute = ({ element, allowedRoles }) => {
   const role = getRole();
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
-// console.log(role , 'role')
-//  if (!role) {
-//   alert(1)
-//   // If user is not logged in, redirect to the login page
-//   navigate('/')
-// }
+ useEffect(()=>{
+  if (role === null){
+    navigate('/');
+   }
+   if(role === 'customer' && !allowedRoles.includes(role)){
+    navigate('/user');
+   }
 
-  // if (allowedRoles && !allowedRoles.includes(user.role)) {
-  //   // If user does not have the required role, redirect based on the user's role
-  //   const redirectPath = user.role === 'admin' ? '/admin' : '/user';
-  //   return <Navigate to={redirectPath} />;
-  // }
+   if(role === 'admin' && !allowedRoles.includes(role)){
+    navigate('/admin');
+   }
+
+   if(role === 'superAdmin' && !allowedRoles.includes(role)){
+    navigate('/admin');
+   }
+ } ,[role])
 
   return element;
 };
