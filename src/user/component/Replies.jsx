6a0c8a45b695f88b9827ea3473b430a0311754a1
shivshaktisingh-cedcommerce <shivshaktisingh@cedcommerce.com>
2024-07-25 17:11:21 +0530

    fixes

--- src/user/component/Replies.jsx
+++ src/user/component/Replies.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 
-import { Govern, GovernifyTeam } from "../../utils/icons";
 import { extractUsernameFromMessage, getFirstLettersOfName, showUserName } from "../../utils/helper";
 
 const Replies = ({ item }) => {
@@ -10,7 +9,7 @@ const Replies = ({ item }) => {
       <div>
         <span
           className="rounded-circle fw-bold text-white d-flex align-items-center justify-content-center"
-          style={{width: "35px", height: "35px" , background:"#497ed8" , paddingTop:userName !== "Onboardify Team"?"3px":"0px" }}
+          style={{width: "35px", height: "35px" , background: userName !== "Onboardify Team"?"#497ed8":'' , paddingTop:userName !== "Onboardify Team"?"3px":"0px" }}
         >
           {userName === "Onboardify Team" ? <img src="/loader.png" alt="No Preview" width={30} height={30}/>  : getFirstLettersOfName(userName)}
         </span>
