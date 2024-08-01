8f56a0e713ff5a92db1e7681ec7a8fb9cb1b323e
shivshaktisingh-cedcommerce <shivshaktisingh@cedcommerce.com>
2024-08-01 12:46:40 +0530

    fixes

--- src/user/Request.jsx
+++ src/user/Request.jsx
@@ -1,36 +1,32 @@
 import React, { useEffect, useState } from "react";
-import Carousel from "react-multi-carousel";
 import { Hero } from "../components/Hero";
-import {
-  getAllProfileDataByUser,
-  getUserFormAndChart,
-} from "../apiservice/ApiService";
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
+import { getAllProfileDataByUser } from "../apiservice/ApiService";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 import { LeftOutlined, PlusOutlined, RightOutlined } from "@ant-design/icons";
+import { Button } from "antd";
 
 export const Request = () => {
   const [formCode, setFormCode] = useState("");
   const [profileData, setProfileData] = useState([]);
 
-  const CustomLeftArrow = ({ onClick }) => (
-    <div className="custom-arrow left" onClick={onClick}>
-      <LeftOutlined />
-    </div>
-  );
-
-  const CustomRightArrow = ({ onClick }) => (
-    <div className="custom-arrow right" onClick={onClick}>
-      <RightOutlined />
-    </div>
-  );
+  const settingsData = JSON.parse(sessionStorage.getItem("settings")) || {
+    image: "https://onboardify.tasc360.com/uploads/y22.png",
+    site_bg: "#ffffff",
+    button_bg: "#497ed8",
+    banner_bg: "#497ed8",
+    banner_content:
+      "Hire an attitude, not just experience and qualification. Greg Savage.",
+    header_bg: "#f7f7f7",
+    head_title_color: "#497ed8",
+  };
 
   const settings = {
     infinite: true,
     speed: 500,
-    slidesToShow: 1,
+    slidesToShow: 4,
     slidesToScroll: 1,
     nextArrow: <CustomArrow arrowType="next" />,
     prevArrow: <CustomArrow arrowType="prev" />,
@@ -41,25 +37,25 @@ export const Request = () => {
           slidesToShow: 1,
           slidesToScroll: 1,
           infinite: true,
-          dots: true
-        }
+          dots: true,
+        },
       },
       {
         breakpoint: 600,
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
-          initialSlide: 1
-        }
+          initialSlide: 1,
+        },
       },
       {
         breakpoint: 480,
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
 
   const fetchProfiledata = async () => {
@@ -90,60 +86,50 @@ export const Request = () => {
           forHome={true}
         />
 
-<div className="carousel-container">
-      <Slider {...settings}>
-        <div>
-          <img src="/1.png" alt="Slide 1" />
-        </div>
-        <div>
-          <img src="/1.png" alt="Slide 2" />
-        </div>
-        <div>
-          <img src="/1.png" alt="Slide 3" />
-        </div>
-      </Slider>
-    </div>
-{/* 
-        <div className="carousel-container">
-          <Carousel
-            arrows
-            dots={false}
-            infinite={true}
-            slidesToShow={4}
-            prevArrow={<CustomLeftArrow />}
-            nextArrow={<CustomRightArrow />}
-          >
+        <div className="carousel-container" style={{ maxWidth: "1200px" }}>
+          <Slider {...settings}>
             {profileData.length > 0 &&
               profileData[0].hasOwnProperty("services") &&
               profileData[0].services.map((item) => {
                 return (
                   <div className="carousel-slide-wrapper" key={item.title}>
                     <div
-                      className="carousel-slide"
                       style={{
-                        display: "flex",
-                        flexDirection: "column",
+                        padding: "20px",
                         paddingBottom: "60px",
                         position: "relative",
                       }}
                     >
-                      <div style={{ width: "263px", borderRadius: "10px" }}>
+                      <div
+                        style={{
+                          maxWidth: "263px",
+                          maxHeight: "170px",
+                          borderRadius: "10px",
+                          overflow: "hidden",
+                        }}
+                      >
                         <img
                           src={item.file_location}
                           alt="No Preview"
-                          style={{ width: "100%", borderRadius: "10px" }}
-                          height={170}
+                          style={{
+                            width: "100%",
+                            borderRadius: "10px",
+                            objectFit: "contain",
+                            height: "auto",
+                          }}
                         />
                       </div>
                       <p
                         style={{
                           textAlign: "left",
                           width: "100%",
+                          maxWidth: "263px",
                           color: "#434343",
                           fontSize: "26px",
                           fontWeight: "700",
                           marginTop: "20px",
                           marginBottom: "0px",
+                          minHeight: "78px",
                         }}
                       >
                         {item.title}
@@ -152,20 +138,31 @@ export const Request = () => {
                         style={{
                           textAlign: "left",
                           width: "100%",
+                          maxWidth: "263px",
                           color: "#928f8f",
                           fontSize: "17px",
                           fontWeight: "400",
                           marginTop: "20px",
                           marginBottom: "0px",
+                          minHeight: "77px",
                         }}
                       >
                         {item.description}
                       </p>
                       <div style={{ width: "100%", textAlign: "left" }}>
                         <Button
-                          icon={<PlusOutlined />}
+                          icon={
+                            <PlusOutlined
+                              style={{ color: settingsData.button_bg }}
+                            />
+                          }
                           iconPosition="start"
-                          style={{ position: "absolute", bottom: "0px" }}
+                          style={{
+                            position: "absolute",
+                            bottom: "0px",
+                            border: `1px solid ${settingsData.button_bg}`,
+                            color: settingsData.button_bg,
+                          }}
                         >
                           Submit Request
                         </Button>
@@ -174,21 +171,37 @@ export const Request = () => {
                   </div>
                 );
               })}
-          </Carousel>
-        </div> */}
+          </Slider>
+        </div>
       </div>
     </div>
   );
 };
 
 const CustomArrow = (props) => {
-  const { className, style, onClick, arrowType } = props;
+  const { className, onClick, arrowType } = props;
+  
+  // Common styles for the arrow container
+  const commonStyle = {
+    paddingRight:arrowType==="next"?"0px":"200px",
+    paddingLeft:arrowType==="prev"?"0px":"200px",
+    borderRadius: "50%", // Makes the container circular
+    cursor: "pointer",
+    zIndex: 2,
+  };
+
   return (
     <div
-      className={`${className} ${arrowType}`}
-      style={{ ...style, display: 'block', background: '#000', borderRadius: '50%' }}
+      className={className}
+      style={commonStyle}
       onClick={onClick}
-    />
+    >
+      {arrowType === "prev" ? (
+        <LeftOutlined style={{ color: "grey", fontSize: "20px" }} /> // Icon color and size
+      ) : (
+        <RightOutlined style={{ color: "grey", fontSize: "20px" }} /> // Icon color and size
+      )}
+    </div>
   );
 };
 
