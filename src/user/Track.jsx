8f5cb7eb6e3a4ba684d587b3a25f040726df6661
shivshaktisingh-cedcommerce <shivshaktisingh@cedcommerce.com>
2024-08-02 15:00:10 +0530

    fixes

--- src/user/Track.jsx
+++ src/user/Track.jsx
@@ -276,6 +276,7 @@ export const Track = () => {
       const response2 = await getColorMappingForUser();
 
       if (response.success) {
+        console.log(response , 'response');
         getFilterColumns(response.data.response.data.boards[0].columns);
         setData(response.data.response.data.boards[0].items_page.items);
         setCursor(response.data.response.data.boards[0].items_page.cursor);
