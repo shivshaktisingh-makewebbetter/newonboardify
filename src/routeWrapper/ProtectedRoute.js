fc589671eaa24f15494204f3ef5f2f8d6de5031d
shivshaktisingh-cedcommerce <shivshaktisingh@cedcommerce.com>
2024-08-06 15:27:11 +0530

    fixes

--- src/routeWrapper/ProtectedRoute.js
+++ src/routeWrapper/ProtectedRoute.js
@@ -1,27 +1,27 @@
 // ProtectedRoute.js
-import { useEffect } from 'react';
-
+import  { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { getRole } from '../utils/helper';
 
 export const ProtectedRoute = ({ element, allowedRoles }) => {
   const role = getRole();
-
+  const navigate = useNavigate();
 
  useEffect(()=>{
-//   if (role === null){
-//     navigate('/');
-//    }
-//    if(role === 'customer' && !allowedRoles.includes(role)){
-//     navigate('/user');
-//    }
+  if (role === null){
+    navigate('/');
+   }
+   if(role === 'customer' && !allowedRoles.includes(role)){
+    navigate('/user');
+   }
 
-//    if(role === 'admin' && !allowedRoles.includes(role)){
-//     navigate('/admin');
-//    }
+   if(role === 'admin' && !allowedRoles.includes(role)){
+    navigate('/admin');
+   }
 
-//    if(role === 'superAdmin' && !allowedRoles.includes(role)){
-//     navigate('/admin');
-//    }
+   if(role === 'superAdmin' && !allowedRoles.includes(role)){
+    navigate('/admin');
+   }
  } ,[role])
 
   return element;
