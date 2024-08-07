c16e3dfad3a2df75e302424fd55ec1f0fda8286d
shivshaktisingh-cedcommerce <shivshaktisingh@cedcommerce.com>
2024-08-07 14:56:55 +0530

    fixes

--- src/user/component/UpdateAndReply.jsx
+++ src/user/component/UpdateAndReply.jsx
@@ -53,7 +53,7 @@ const UpdateAndReply = ({
         <div className="d-flex align-items-center">
           <span
             className="rounded-circle fw-bold text-white d-flex align-items-center justify-content-center"
-            style={{ width: "40px", height: "40px", background: userName !== "Onboardify Team"?"#497ed8":'' , paddingTop:userName !== "Onboardify Team"?"4px":"0px" , fontSize:"18px" }}
+            style={{ width: "40px", height: "40px", background: "#497ed8" , paddingTop:userName !== "Onboardify Team"?"4px":"0px" , fontSize:"18px" }}
           >
             {userName === "Onboardify Team" ? (
              <img src="/onboard123.svg" alt="No Preview" width={30} height={30}/> 
