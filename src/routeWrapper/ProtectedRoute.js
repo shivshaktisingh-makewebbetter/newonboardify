fe89fbeae315dac52aadc4af43cd16df76c160f4
shivshaktisingh-cedcommerce <shivshaktisingh@cedcommerce.com>
2024-10-24 17:56:18 +0530

    fixes

--- src/routeWrapper/ProtectedRoute.js
+++ src/routeWrapper/ProtectedRoute.js
@@ -1,5 +1,5 @@
 // ProtectedRoute.js
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { getRole } from '../utils/helper';
 
